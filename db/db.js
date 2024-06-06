25be01345b5df0aff36aab01d2ef0d4c4fc88511
Bawantha Bandara <133810549+Bawantha1234@users.noreply.github.com>
2024-06-06 20:13:06 +0530

    update

--- db/db.js
+++ db/db.js
@@ -1,22 +1,3 @@
 
 export var customer_db = [];
 
-export function searchItem(id){
-    let item = localStorage.getItem(itemDb);
-    let parse = JSON.parse(item);
-    return parse.find(obj=>obj.itemCode==id)
-}
-
-export function getAllItem(){
-    return JSON.parse(localStorage.getItem(itemDb));
-}
-
-export function searchCustomer(id){
-    let item = localStorage.getItem(customerDb);
-    let parse = JSON.parse(item);
-    return parse.find(obj=>obj._id==id)
-}
-
-export function getAllCustomers(){
-    return JSON.parse(localStorage.getItem(customerDb));
-}
